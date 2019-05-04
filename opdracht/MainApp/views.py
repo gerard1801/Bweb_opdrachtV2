43b53b61bd229eb3dfc66d4000f9fbaead12a9a7
gerard1801 <gerard_vanderwel@hotmail.com>
2019-05-04 16:41:48 +0200

    match stats

--- opdracht/MainApp/views.py
+++ opdracht/MainApp/views.py
@@ -3,21 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-import sqlite3
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 from scipy.misc import imread
 import matplotlib.cbook as cbook
-from django.db import connection
 from bokeh.plotting import figure, output_file, show, save
 from bokeh.models import OpenURL, TapTool, ColumnDataSource, HoverTool
 from bokeh.models.callbacks import CustomJS
-from bokeh.embed import components
-from bokeh.resources import INLINE
-from bokeh.resources import CDN
-from bokeh.embed import file_html
+
 
 import sqlite3
 
@@ -59,7 +54,6 @@ def Club(request):
             global match_id
             match_id = request.POST['match-btn'][:]
             print(match_id)
-            request.session['match_id'] = match_id
             return HttpResponseRedirect('/match')
 
 
@@ -77,10 +71,6 @@ def Match(request):
     cur = conn.cursor()
     print("match_ID")
     print(match_id)
-    tables = pd.read_sql("""SELECT *
-                                    FROM Match 
-                                    WHERE match_api_id=1988797;""", conn)
-    match_api_id = 1988797
     sql = 'SELECT * From MATCH WHERE match_api_id=?'
     cur.execute(sql, (match_id,))
     match = cur.fetchone()
@@ -141,215 +131,153 @@ def Match(request):
     output_file("templates/bokeh2.html")
     save(p)
 
+    #match gegevens
+    conn = sqlite3.connect('/Users/gerardvanderwel/Downloads/database.sqlite')
+    c = conn.cursor()
+    thuis_goals = c.execute("SELECT home_team_goal FROM MATCH WHERE match_api_id=?", (match_id,))
+    thuis_goals = thuis_goals.fetchall()
+    print(thuis_goals)
+    uit_goals = c.execute("SELECT away_team_goal FROM MATCH WHERE match_api_id=?", (match_id,))
+    uit_goals = uit_goals.fetchall()
+    print(uit_goals)
+    thuis_team = c.execute("SELECT team_long_name FROM Team WHERE team_api_id IN(SELECT home_team_api_id from Match WHERE match_api_id=?)", (match_id,))
+    thuis_team = thuis_team.fetchall()
+    uit_team = c.execute("SELECT team_long_name FROM Team WHERE team_api_id IN(SELECT away_team_api_id from Match WHERE match_api_id=?)",(match_id,))
+    uit_team = uit_team.fetchall()
+    thuis_api = c.execute("SELECT home_team_api_id from Match WHERE match_api_id=?", (match_id,))
+    thuis_api = thuis_api.fetchall()
+    uit_api = c.execute("SELECT away_team_api_id from Match WHERE match_api_id=?", (match_id,))
+    uit_api = uit_api.fetchall()
 
-    return render(request, 'Match.html')
+#fouls berekenen
+    fouls = c.execute("SELECT foulcommit FROM Match WHERE match_api_id=?", (match_id,))
+    fouls = fouls.fetchall()
+    fouls_str = fouls[0][0]
+    fouls_str = fouls_str.split("<team>")
+    new_list = []
+    for x in fouls_str:
+        new_list.append(x.split("</team>"))
+    new_list.pop(0)
+    for i in range(len(new_list)):
+        new_list[i].pop(1)
+    lijst = [' '.join([str(c) for c in lst]) for lst in new_list]
+    thuis_fouls = 0
+    uit_fouls = 0
+    for i in lijst:
+        if i == str(thuis_api[0][0]):
+            thuis_fouls += 1
+        elif i == str(uit_api[0][0]):
+            uit_fouls += 1
+
+#shots on target berekenen
+    shoton = c.execute("SELECT shoton FROM Match WHERE match_api_id=?", (match_id,))
+    shoton = shoton.fetchall()
+    shoton_str = shoton[0][0]
+    shoton_str = shoton_str.split("<team>")
+    shot_list = []
+    for x in shoton_str:
+        shot_list.append(x.split("</team>"))
+    shot_list.pop(0)
+    for i in range(len(shot_list)):
+        shot_list[i].pop(1)
+    lijst2 = [' '.join([str(c) for c in lst]) for lst in shot_list]
+    thuis_shots = 0
+    uit_shots = 0
+    for i in lijst2:
+        if i == str(thuis_api[0][0]):
+            thuis_shots += 1
+        elif i == str(uit_api[0][0]):
+            uit_shots += 1
+
+#shots off target berekenen
+    shotoff = c.execute("SELECT shotoff FROM Match WHERE match_api_id=?", (match_id,))
+    shotoff = shotoff.fetchall()
+    shotoff_str = shotoff[0][0]
+    shotoff_str = shotoff_str.split("<team>")
+    shotoff_list = []
+    for x in shotoff_str:
+        shotoff_list.append(x.split("</team>"))
+    shotoff_list.pop(0)
+    for i in range(len(shotoff_list)):
+        shotoff_list[i].pop(1)
+    lijst3 = [' '.join([str(c) for c in lst]) for lst in shotoff_list]
+    thuis_shotsoff = 0
+    uit_shotsoff = 0
+    for i in lijst3:
+        if i == str(thuis_api[0][0]):
+            thuis_shotsoff += 1
+        elif i == str(uit_api[0][0]):
+            uit_shotsoff += 1
+
+#crosses per team
+    crosses = c.execute("SELECT cross FROM Match WHERE match_api_id=?", (match_id,))
+    crosses = crosses.fetchall()
+    crosses_str = crosses[0][0]
+    crosses_str = crosses_str.split("<team>")
+    crosses_list = []
+    for x in crosses_str:
+        crosses_list.append(x.split("</team>"))
+    crosses_list.pop(0)
+    for i in range(len(crosses_list)):
+        crosses_list[i].pop(1)
+    lijst4 = [' '.join([str(c) for c in lst]) for lst in crosses_list]
+    thuis_crosses = 0
+    uit_crosses = 0
+    for i in lijst4:
+        if i == str(thuis_api[0][0]):
+            thuis_crosses += 1
+        elif i == str(uit_api[0][0]):
+            uit_crosses += 1
+
+#corners per team
+    corners = c.execute("SELECT corner FROM Match WHERE match_api_id=?", (match_id,))
+    corners = corners.fetchall()
+    corners_str = corners[0][0]
+    print("corners")
+    print(corners_str)
+    corners_str = corners_str.split("<team>")
+    print(corners_str)
+    corners_list = []
+    for x in corners_str:
+        corners_list.append(x.split("</team>"))
+    corners_list.pop(0)
+    for i in range(len(corners_list)):
+        corners_list[i].pop(1)
+    lijst5 = [' '.join([str(c) for c in lst]) for lst in corners_list]
+    thuis_corners = 0
+    uit_corners = 0
+    for i in lijst5:
+        if i == str(thuis_api[0][0]):
+            thuis_corners += 1
+        elif i == str(uit_api[0][0]):
+            uit_corners += 1
+
+    print(corners)
+    print(corners_list)
+    print(thuis_corners, uit_corners)
+
+    return render(request, 'Match.html', {
+        'thuis_goals': thuis_goals,
+        'uit_goals': uit_goals,
+        'thuis_team': thuis_team,
+        'uit_team': uit_team,
+        'thuis_fouls': thuis_fouls,
+        'uit_fouls': uit_fouls,
+        'thuis_shotson': thuis_shots,
+        'uit_shotson': uit_shots,
+        'thuis_shotsoff': thuis_shotsoff,
+        'uit_shotsoff': uit_shotsoff,
+        'thuis_crosses': thuis_crosses,
+        'uit_crosses': uit_crosses,
+        'thuis_corners': thuis_corners,
+        'uit_corners': uit_corners,
+    })
 
 def Player(request):
     return render(request, 'Player.html')
 
 def Home(request):
-    return render(request, 'bokeh.html')
+    return render(request, 'Home.html')
 
 
-
-
-
-
-
-
-def Opstelling(request):
-    database = '/Users/gerardvanderwel/Downloads/database.sqlite'
-    conn = sqlite3.connect(database)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    tables = pd.read_sql("""SELECT *
-                            FROM Match 
-                            WHERE match_api_id=1989903;""", conn)
-    match_api_id = 1989903
-    sql = 'SELECT * From MATCH WHERE match_api_id=?'
-    cur.execute(sql, (match_api_id,))
-    match = cur.fetchone()
-
-    home_players_api_id = list()
-    away_players_api_id = list()
-    home_players_x = list()
-    away_players_x = list()
-    home_players_y = list()
-    away_players_y = list()
-
-    for i in range(1, 12):
-        home_players_api_id.append(match['home_player_%d' % i])
-        away_players_api_id.append(match['away_player_%d' % i])
-        home_players_x.append(match['home_player_X%d' % i])
-        away_players_x.append(match['away_player_X%d' % i])
-        home_players_y.append(match['home_player_Y%d' % i])
-        away_players_y.append(match['away_player_Y%d' % i])
-
-    print('Example, home players api id: ')
-    print(home_players_api_id)
-
-    players_api_id = [home_players_api_id, away_players_api_id]
-    players_api_id.append(home_players_api_id)  # Home
-    players_api_id.append(away_players_api_id)  # Away
-    players_names = [[None] * 11, [None] * 11]
-
-    for i in range(2):
-        players_api_id_not_none = [x for x in players_api_id[i] if x is not None]
-        sql = 'SELECT player_api_id,player_name FROM Player'
-        sql += ' WHERE player_api_id IN (' + ','.join(map(str, players_api_id_not_none)) + ')'
-        cur.execute(sql)
-        players = cur.fetchall()
-        for player in players:
-            idx = players_api_id[i].index(player['player_api_id'])
-            name = player['player_name'].split()[-1]  # keep only the last name
-            players_names[i][idx] = name
-
-    print('Home team players names:')
-    print(players_names[0])
-    print('Away team players names:')
-    print(players_names[1])
-
-    home_players_x = [5 if x == 1 else x for x in home_players_x]
-    away_players_x = [5 if x == 1 else x for x in away_players_x]
-
-
-    # Home team (in blue)
-    plt.subplot(2, 1, 1)
-    plt.rc('grid', linestyle="-", color='black')
-    plt.rc('figure', figsize=(12, 20))
-    plt.gca().invert_yaxis()  # Invert y axis to start with the goalkeeper at the top
-    for label, x, y in zip(players_names[0], home_players_x, home_players_y):
-        plt.annotate(
-            label,
-            xy=(x, y), xytext=(-20, 20),
-            textcoords='offset points', va='bottom')
-    plt.scatter(home_players_x, home_players_y, s=100, c='blue')
-    plt.grid(True)
-
-    datafile = cbook.get_sample_data(
-        '/Users/gerardvanderwel/PycharmProjects/Bweb_opdrachtV2/opdracht/static/images/Soccer_Field_Transparant.png')
-    img = imread(datafile)
-    #plt.scatter(x, y, zorder=1)
-    plt.imshow(img, zorder=0, extent=[1.0, 9.0, 1.0, 7.0])
-    # plt.show()
-    plt.savefig("./static/images/line-up2.png")
-
-    # Away team (in red)
-    plt.subplot(2, 1, 2)
-    plt.rc('grid', linestyle="-", color='black')
-    plt.rc('figure', figsize=(12, 20))
-    plt.gca().invert_xaxis()  # Invert x axis to have right wingers on the right
-    for label, x, y in zip(players_names[1], away_players_x, away_players_y):
-        plt.annotate(
-            label,
-            xy=(x, y), xytext=(-20, 20),
-            textcoords='offset points', va='bottom')
-    plt.scatter(away_players_x, away_players_y, s=480, c='red')
-    plt.grid(True)
-
-    ax = [plt.subplot(2, 2, i + 1) for i in range(0)]
-    for a in ax:
-        a.set_xticklabels([])
-        a.set_yticklabels([])
-    plt.subplots_adjust(wspace=0, hspace=0)
-    plt.savefig("./static/images/line-up.png")
-
-
-    players_y = [home_players_y, away_players_y]
-    formations = [None] * 2
-    for i in range(2):
-        formation_dict = Counter(players_y[i]);
-        sorted_keys = sorted(formation_dict)
-        formation = ''
-        for key in sorted_keys[1:-1]:
-            y = formation_dict[key]
-            formation += '%d-' % y
-        formation += '%d' % formation_dict[sorted_keys[-1]]
-        formations[i] = formation
-
-    print('Home team formation: ' + formations[0])
-    print('Away team formation: ' + formations[1])
-
-    np.random.seed(0)
-    x = np.random.uniform(0.0, 10.0, 15)
-    y = np.random.uniform(0.0, 10.0, 15)
-
-
-
-    return render(request, 'Match.html')
-
-
-def test(request):
-    database = '/Users/gerardvanderwel/Downloads/database.sqlite'
-    conn = sqlite3.connect(database)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    tables = pd.read_sql("""SELECT *
-                                FROM Match 
-                                WHERE match_api_id=1988797;""", conn)
-    match_api_id = 1988797
-    sql = 'SELECT * From MATCH WHERE match_api_id=?'
-    cur.execute(sql, (match_api_id,))
-    match = cur.fetchone()
-    print(match)
-
-    home_players_api_id = list()
-    away_players_api_id = list()
-    home_players_x = list()
-    away_players_x = list()
-    home_players_y = list()
-    away_players_y = list()
-
-    for i in range(1, 12):
-        home_players_api_id.append(match['home_player_%d' % i])
-        away_players_api_id.append(match['away_player_%d' % i])
-        home_players_x.append(match['home_player_X%d' % i])
-        away_players_x.append(match['away_player_X%d' % i])
-        home_players_y.append(match['home_player_Y%d' % i])
-        away_players_y.append(match['away_player_Y%d' % i])
-
-    players_api_id = [home_players_api_id, away_players_api_id]
-    players_api_id.append(home_players_api_id)  # Home
-    players_api_id.append(away_players_api_id)  # Away
-    players_names = [[None] * 11, [None] * 11]
-
-
-    for i in range(2):
-        players_api_id_not_none = [x for x in players_api_id[i] if x is not None]
-        sql = 'SELECT player_api_id,player_name FROM Player'
-        sql += ' WHERE player_api_id IN (' + ','.join(map(str, players_api_id_not_none)) + ')'
-        cur.execute(sql)
-        players = cur.fetchall()
-        for player in players:
-            idx = players_api_id[i].index(player['player_api_id'])
-            name = player['player_name'].split()[-1]  # keep only the last name
-            players_names[i][idx] = name
-
-    home_players_x = [5 if x == 1 else x for x in home_players_x]
-    away_players_x = [5 if x == 1 else x for x in away_players_x]
-
-    datafile = cbook.get_sample_data(
-        '/Users/gerardvanderwel/PycharmProjects/Bweb_opdrachtV2/opdracht/static/images/Soccer_Field_Transparant.png')
-
-
-    home_plot_x = []
-    home_plot_y = []
-    for i in home_players_x:
-        home_plot_x.append(i * 50)
-    for i in home_players_y:
-        home_plot_y.append(i * 50)
-
-    output_file("line.html")
-    p = figure(title="opstelling", x_range=(0,500), y_range=(0,600), tools="tap")
-    p.image_url(url=['/static/images/Soccer_Field_Transparant.png'], x=0, y=600, w=500, h=600)
-    p.circle(home_plot_x, home_plot_y, size=20)
-    url = "Player.html"
-    taptool = p.select(type=TapTool)
-    taptool.callback = CustomJS(code="""          
-            window.open("http://127.0.0.1:8000/player" ,"_self");
-            """)
-    output_file("templates/bokeh2.html")
-    save(p)
-
-    return render(request, 'Player.html')
 
