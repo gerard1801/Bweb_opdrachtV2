f667ae977ccea5002e38dd24439811eb067972e6
gerard1801 <43404416+gerard1801@users.noreply.github.com>
2021-03-19 15:54:07 +0100

    Update settings.py

--- opdracht/opdracht/settings.py
+++ opdracht/opdracht/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+--$z^w6ld07c2u7o4(2^1hq5=+vyb%9alqe7-3$tpd42g0&fy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
